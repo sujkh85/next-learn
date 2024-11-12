d08f46b38a5e481b3c4ed031369d3fc13431157b
sujkh <sujkh@ncsoft.com>
2024-11-12 15:43:23 +0900

    initail

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], preload: true })
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
